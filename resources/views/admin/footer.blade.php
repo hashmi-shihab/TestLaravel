54304e5a9a7eddc1c4a73e27592e9f722d543fff
hashmishihab <43649408+hashmishihab@users.noreply.github.com>
2020-05-09 12:57:58 +0600

    renamed

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2019 <a href="http://g8ict.com">#SELFTAG</a> LIMITED</strong> All rights
+    <strong>Copyright  &copy; {{Carbon\Carbon::now()->format('yy')}} <a href="{{route('home')}}">#SELFTAG</a> LIMITED</strong> All rights
     reserved.
 </footer>
 
--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,11 +1,12 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="#" class="logo">
-
-        <span class="logo-mini">LaravelUserRole</span>
-
-        <span class="logo-lg">LaravelUserRole</span>
+    <a href="{{route('home')}}" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>L</b>TA</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>LaravelTest</b>App</span>
     </a>
+    <!-- Header Navbar: style can be found in header.less -->
 
     <nav class="navbar navbar-static-top">
 
